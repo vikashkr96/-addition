1a6412ae9ad2168ceb08407690214f7a5e8ba3d2
Vikash Kumar <vk4845646@gmail.com>
2024-09-30 10:41:54 +0530

    Add files via upload

---
+++ c++ leanings/userinput.cpp
@@ -0,0 +1,14 @@
+// Program to add two numbers and then add them
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cout<<"Enter the first number:";
+    cin>>a;
+    cout<<"Enter the second number:";
+    cin>>b;
+    int c=a+b;
+    cout<<"The sum is: "<<c <<endl;
+    
+    return 0;
+}
